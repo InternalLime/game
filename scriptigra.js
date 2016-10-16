33f143255080590085039d33a9dd76f7d8fcd0b4
Lime <ivan8h8@gmail.com>
2016-10-16 23:51:42 +0300

    удалил лишние файлы, и набросок матрицы

--- scriptigra.js
+++ scriptigra.js
@@ -1,11 +1,22 @@
-var Matri
-var a = new Array();
-a[0] = 1.2;
-a[1] = "JavaScript";
-a[2] = true;
-a[3] = { x:1, y:3 };
-
-
-
-
-
+var c="";
+function matrixArray(rows,columns)
+{
+  var arr = new Array();
+  for(var y=0; x<columns; i++)
+  {
+    arr[y] = new Array();
+    for(var x=0; x<rows; j++)
+    {
+      arr[y][x] = 0;
+      c=+ ", 0";
+    }
+  }
+  return arr;
+}
+function proverka()
+{
+var myMatrix = matrixArray(3,3);
+alert(c);
+document.getElementById("inputValue").value = c;  
+}
+ proverka();
