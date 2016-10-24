79e5fc86162c0adf2009c20d1ceb1a5096a466df
Lime <ivan8h8@gmail.com>
2016-10-24 20:58:11 +0300

    мелкие исправления замечаний

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 var x=10;
 var y=10;
 var matrix=""; 
-var inspect = ""; 
+var inspect = ""; var insp = ""; 
 var gamer = 2;
 var Xscan = 1;
 var Yscan = 1;
 var score = 0; 
+var field_width = 10;
+var height_field = 10; 
+var limit_of_ones=0; 
+
+function input() 
+{
+  matrixArray(field_width,height_field);
+  document.onkeydown = function(evt) 
+  {
+    evt = evt || window.event;
+    if (evt.ctrlKey && evt.keyCode == 37)
+    {
+        alert(37);
+    }
+    if (evt.ctrlKey && evt.keyCode == 38)
+    {
+        funcup();
+    } 
+       if (evt.ctrlKey && evt.keyCode == 39)
+    {
+        funcright();
+    }  
+      if (evt.ctrlKey && evt.keyCode == 40)
+    {
+        funcdown();
+    }
+  }
+}
 
 function matrixArray(rows,columns)
 {
@@ -16,43 +44,42 @@ function matrixArray(rows,columns)
     for(y=0; y<rows; y++)
     {
       matrix[x][y] = Math.floor((Math.random() *2)); 
-
     } 
 
     print(matrix);
   }
   return matrix;
 }
+
 function print(m)
 {
-   m = new Array();
-  for(x=0; x<10; x++)
+  for(x=0; x<field_width; x++)
   {
-    m[x] = new Array();
-    for(y=0; y<10; y++)
+    m[x] = Array();
+    for(y=0; y<height_field; y++)
     {
-      m[x][y] = Math.floor((Math.random() *2)); 
+      m[x][y] = Math.floor((Math.random() *2));
       inspect += m[x][y] + ", "; 
     } 
     inspect += "\n";
     document.getElementById("Text1").value = inspect; 
   }
-  return matrix;
 }
 
- function  funcleft()
+ function  funcup()
  {
-   document.getElementById("inputValue").value = "37";  
+   document.getElementById("inputValue").value = "38";  
  }
 
 function scan()
 {
   document.getElementById("inputValue").value = "X="+ Xscan + "Y=" + Yscan;
-  if (matrix[Xscan][Yscan] == 1) //как это записать??
+  x = Xscan;
+  y = Yscan;
+  if (matrix[x][y] == 1)
   {
     score = score + 1;
-    document.getElementById("textscore").value = score; 
+    document.getElementById("textscore").value = score;  
     matrix[x][y] =0; //Как это записать???
   }
-
 }
