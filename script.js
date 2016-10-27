cb54e8af8f9a8a666f94ef61082b330ab6ab3e42
Lime <ivan8h8@gmail.com>
2016-10-27 17:13:03 +0300

    переработал скан,  создание матрицы,  добавил нажатия кнопок, визуальную часть, прочие исправления

--- script.js
+++ script.js
@@ -1,62 +1,190 @@
 var matrix=""; 
-var inspect = ""; var insp = ""; 
-var gamer = 2;
+var inspect = "";
 var Xscan = 1;
 var Yscan = 1;
 var score = 0; 
-var field_width = 10;
-var height_field = 10; 
-var limit_of_ones=0; 
+var field_width = 11;
+var height_field = 11; 
+var coordinates = "1,1";
+var a =0;
+var b = 0;
 
 function input() 
 {
   matrixArray(field_width,height_field);
   print(matrix);
+  document.onkeydown = function(evt) 
+  {
+     if (evt.keyCode == 37)
+    {
+        func37();
+    }
+    if (evt.keyCode == 38)
+    {
+        func38();
+    } 
+       if (evt.keyCode == 39)
+    {
+        func39();
+    }  
+      if (evt.keyCode == 40)
+    {
+        func40();
+    }
+  }
 }
 
 function matrixArray(rows,columns)
 {
-   matrix = new Array();
+  matrix = new Array();
   for( var x=0;  x<columns; x++)
   {
     matrix[x] = new Array();
     for( var y=0; y<rows; y++)
     {
-      matrix[x][y] = Math.floor((Math.random() *2)); 
+      matrix[x][y] = 0;
     } 
+  }
+  for( var u=0; u<41; u++)
+  {
+    i = Math.floor((Math.random() *11)); 
+    p = Math.floor((Math.random() *11)); 
+    if (matrix[i][p] == 0)
+    {
+      matrix[i][p] = 1;
+    }
+    else
+    {
+      if(u==0)
+      {
+      }
+      else
+      {
+        u= u-1;
+      }
+    }
   }
   return matrix;
 }
 
 function print(m)
 {
-
-  for(var a = 1; a<field_width; a++)
+  inspect="";
+  document.getElementById("Text1").value = ""; 
+  for(a = 1; a<field_width; a++)
   {
-    for(var b = 1; b<height_field; b++)
-    {
+    for(b = 1; b<height_field; b++)
+    {     
       inspect += matrix[a][b]; 
       inspect += " "; 
+      money();
     } 
     inspect += "\n";
   }
-
-  document.getElementById("Text1").value = inspect;   
+  document.getElementById("Text1").value = inspect;  
 }
 
 function scan()
 {
-  document.getElementById("inputValue").value = "X="+ Xscan + "Y=" + Xscan;
+  guy();
   if (matrix[Xscan][Yscan] == 1)
   {
     score = score + 1;
     document.getElementById("textscore").value = score;  
     matrix[Xscan][Yscan] =0;
-    document.getElementById("Text1").value = ""; 
     print(matrix);
+  }
+  else
+  {
+  score = score + 0; 
+  document.getElementById("textscore").value = score;  
   }
 }
 
+ function func39()
+{
+  if ( Yscan == 10)
+  {
+    document.getElementById("inputValue").value = "X="+ Xscan + "Y=" + Yscan;
+    scan();
+  }
+   else
+   {
+        del_guy();
+    Yscan = Yscan + 1;
+    document.getElementById("inputValue").value = "X="+ Xscan + "Y=" + Yscan;
+    scan();
+   }
+}
 
+ function func37()
+{
 
+  if ( Yscan == 1)
+  {
+    document.getElementById("inputValue").value = "X="+ Xscan + "Y=" + Yscan;
+    scan();
+  }
+   else
+   {
+    del_guy();
+    Yscan = Yscan -1;
+    document.getElementById("inputValue").value = "X="+ Xscan + "Y=" + Yscan;
+    scan();
+   }
+}
+ function func40()
+{
 
+  if ( Xscan == 10)
+  {
+    document.getElementById("inputValue").value = "X="+ Xscan + "Y=" + Yscan;
+    scan();
+  }
+   else
+   {
+        del_guy();
+    Xscan = Xscan + 1;
+    document.getElementById("inputValue").value = "X="+ Xscan + "Y=" + Yscan;
+    scan();
+   }
+}
+ function func38()
+{
+;
+  if ( Xscan == 1)
+  {
+    document.getElementById("inputValue").value = "X="+ Xscan + "Y=" + Yscan;
+    scan();
+  }
+   else
+   {
+    del_guy();
+    Xscan = Xscan - 1;
+    document.getElementById("inputValue").value = "X="+ Xscan + "Y=" + Yscan;
+    scan();
+   }
+}
+ function money()
+{
+  if (matrix[a][b] == 1)
+  {
+    coordinates = a+","+b;
+    document.getElementById(coordinates).src="2.png";
+    coordinates = 0;
+  }
+}
+
+ function guy()
+{
+  coordinates = Xscan+","+Yscan;
+  document.getElementById(coordinates).src="3.png";
+  coordinates = 0;
+}
+
+  function del_guy()
+  {
+  coordinates = Xscan+","+Yscan;
+  document.getElementById(coordinates).src="1.png";
+  coordinates = 0;
+}
